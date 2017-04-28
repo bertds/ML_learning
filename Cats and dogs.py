573f454417eb24be6b03a603c44e311f152444cb
bertds <bert.desutter@gmail.com>
2017-04-28 16:53:00 +0200

    init

---
+++ Cats and dogs.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+import os
+from random import shuffle
+from tqdm import tqdm
+
+TRAIN_DIR = 'H:/Downloads/train'
+TEST_DIR = 'H:/Downloads/test'
+IMG_SIZE = 50
+LR = 10-3
+
+MODEL_NAME = 'dogsvcats-{}-{}.model'.format(LR, '2conv-basic')
+
+def label_img(img):
+    word_label = img.split('.')[-3]
+    if word_label == 'cat': return [1,0]
+    elif word_label == 'dog' : return [0,1]
+
+
+def create_train_data():
+    training_data = []
+    for img in tqdm(os.listdir(TRAIN_DIR)):
+        label = label_img(img)
+        path = os.path.join(TRAIN_DIR,img)
+        img = cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE), (IMG_SIZE,IMG_SIZE))
+        training_data.append([np.array(img), np.array(label)])
+    shuffle(training_data)
+    np.save('train_data.npy',training_data)
+    return training_data
+
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+
+convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+
+convnet = conv_2d(convnet, 32, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = dropout(convnet, 0.8)
+
+convnet = fully_connected(convnet, 2, activation='softmax')
+convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+#train_data = create_train_data()
